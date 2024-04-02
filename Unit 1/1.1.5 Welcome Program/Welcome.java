7f2f7b59bd2e5a16c9750c975e53a01b4fe5d4ec
StudyingApHard <165826293+StudyingApHard@users.noreply.github.com>
2024-04-02 12:18:22 -0500

    Create Welcome.java

---
+++ Unit 1/1.1.5 Welcome Program/Welcome.java
@@ -0,0 +1,8 @@
+public class Welcome 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("My name is Student");
+        System.out.print("My favorite hobby is Coding");
+    } 
+}
